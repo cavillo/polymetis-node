8f4fa3ce1627b9d22fb5e1bf745e0ba73ac06479
Carlos Asmadt <cavillo@gmail.com>
2019-06-03 20:47:50 -0400

    exporting massive db intance

--- dist/postgres/index.d.ts
+++ dist/postgres/index.d.ts
@@ -1,11 +1,11 @@
-import massive from 'massive';
-export declare type DatabaseInstance = massive.Database;
+import { Database as DatabaseInstance } from 'massive';
+export { DatabaseInstance, };
 import { PostgresConfiguration } from '../utils/ServiceConf';
 import Logger from '../utils/Logger';
 export default class Postgres {
     protected conf: PostgresConfiguration;
     protected logger: Logger;
-    protected instance: DatabaseInstance;
+    instance: DatabaseInstance;
     constructor(conf: PostgresConfiguration, logger: Logger);
     init(): Promise<void>;
     dbInstance(force?: boolean): Promise<DatabaseInstance>;
