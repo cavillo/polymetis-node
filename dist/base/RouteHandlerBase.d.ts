0b48585bfd417053b69b186f95ecc969187df0c4
Víctor Gabriel Moreno <victorgmp@gmail.com>
2019-09-27 13:40:22 -0400

    add returned types in .base.ts and rabbit

--- dist/base/RouteHandlerBase.d.ts
+++ dist/base/RouteHandlerBase.d.ts
@@ -3,7 +3,7 @@ import Base from './.base';
 export default abstract class RouteBase extends Base {
     url: string;
     routeCallback(req: Request, res: Response): Promise<any>;
-    callRPC(service: string, procedure: string, data: any): Promise<any>;
+    callRPC(service: string, procedure: string, data: any): Promise<import("../rabbit").RPCResponsePayload>;
     protected abstract callback(req: Request, res: Response): Promise<any>;
     protected detectKnownErrors(error: Error, res: Response): Promise<import("express-serve-static-core").Response>;
 }
--- src/base/.base.ts
+++ src/base/.base.ts
@@ -1,11 +1,12 @@
 import { ServiceResources } from '../';
+import { RPCResponsePayload } from '../rabbit';
 
 export default abstract class Base {
   constructor(protected resources: ServiceResources) {
     this.resources = resources;
   }
 
-  public async callRPC(service: string, procedure: string, data: any) {
+  public async callRPC(service: string, procedure: string, data: any): Promise<RPCResponsePayload> {
     const { environment } = this.resources.configuration.service;
     const topic = `${environment}.${service}.rpc.${procedure}`;
     return this.resources.rabbit.callProcedure(
