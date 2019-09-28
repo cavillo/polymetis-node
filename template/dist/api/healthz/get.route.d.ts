2834f1267eba937422010d309ae5634def5c2ae7
Víctor Gabriel Moreno <victorgmp@gmail.com>
2019-09-28 13:12:52 -0400

    remove changes in template folder

--- template/dist/api/healthz/get.route.d.ts
+++
@@ -1,6 +0,0 @@
-import { Request, Response, ApiRoute, ServiceResources } from 'polymetis-node';
-export default class ApiRouteImpl extends ApiRoute {
-    url: string;
-    constructor(resources: ServiceResources);
-    callback(req: Request, res: Response): Promise<any>;
-}
--- template/dist/tasks/internal/healthz.task.d.ts
+++
@@ -1,6 +0,0 @@
-import { ServiceResources, TaskHandlerBase } from 'polymetis-node';
-export default class Handler extends TaskHandlerBase {
-    topic: string;
-    constructor(resources: ServiceResources);
-    protected handleCallback(data: any): Promise<void>;
-}
